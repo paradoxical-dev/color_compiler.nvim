1333078e7141e642ed5c9802dc7104a460385c01
paradoxical-dev <jaredonnell21@gmail.com>
2024-12-18 19:33:06 -0600

    Moved groups to external file / added custom groups option to user command

--- lua/color-compiler/init.lua
+++ lua/color-compiler/init.lua
@@ -37,13 +37,19 @@ vim.api.nvim_create_user_command("ColorCompiler", function(opts)
 	local args = vim.split(opts.args, " ")
 	local theme = args[1]
 	local bg = args[2] or vim.o.background
+	local custom_groups = {}
+	if #args <= 3 then
+		for i = 3, #args do
+			table.insert(custom_groups, args[i])
+		end
+	end
 
 	if not theme then
 		print("color-compiler (error): Theme name is required.")
 		return
 	end
 
-	require("color-compiler.compiler").compile(theme, bg)
+	require("color-compiler.compiler").compile(theme, bg, custom_groups)
 	print("Successfully compiled theme: " .. theme .. " into " .. path .. theme)
 end, {
 	nargs = "*",
